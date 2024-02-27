f93544e0a5c6e33b22473184a1a79efd2f563e71
Echono <wonger1324@gmail.com>
2024-02-28 00:18:33 +0100

    Changed to SlashCommandBuilder

--- src/controllers/CommandsController.js
+++ src/controllers/CommandsController.js
@@ -5,20 +5,29 @@ const path = require('path');
 const debug = require('debug')('CommandsController');
 
 const registerComamnds = () => {
-    debug('Gathering file directories');
-    const appdir = path.dirname(require.main.filename);
-    const filedir = path.join(appdir, 'models/commands.json');
-    const filestream = fs.readFileSync(filedir);    
-    const json = JSON.parse(filestream);
-    debug(`Found ${json.commands.length} ${json.commands.length > 1 ? "commands" : "command"}`);
-    if(json.commands.length > 0) {
+    const commands = [];
+    const folderdir = path.join(path.dirname(require.main.filename), "slash_commands");
+    const folder = fs.readdirSync(folderdir).filter(file => file.endsWith('.js'));
+
+    debug(`Found ${folder.length} ${folder.length > 1 ? "commands" : "command"}`)
+    for (const file of folder) {
+        const filedir = path.join(folderdir, file);
+        const command = require(filedir);
+        if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			debug(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+    }
+
+    if(commands.length > 0) {
         const rest = new REST().setToken(process.env.BOT_TOKEN);
         (async () => {
             try {
                 debug('Attempting to update bot commands');
                 const data = await rest.put(
                     Routes.applicationCommands(process.env.APP_ID),
-                    { body: json.commands }
+                    { body: commands }
                 )
                 debug(`Successfully registered: ${data.length} ${data.length > 1 ? "commands" : "command"}`);
             } catch(error) {   
