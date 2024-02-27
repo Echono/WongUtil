ed21b535a2f4af54c9cae17ec50d9b40a79d48f9
Echono <wonger1324@gmail.com>
2024-02-27 09:16:43 +0100

    Verification ping/pong

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY)}));
 
-app.get('/interactions', (req, res) => {
+app.get('/interactions', async (req, res) => {
     const { type, id, data } = req.body;
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG });
+    }
 })
 
 app.listen(PORT, () => {
