c2fabbe813f0b04bb216c890565e7556d5533385
Echono <wonger1324@gmail.com>
2024-02-28 13:25:01 +0100

    First iteration of working unsubscribe

--- src/slash_commands/unsubscribe.js
+++ src/slash_commands/unsubscribe.js
@@ -1,7 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
+const debug = require('debug')('slashCommand_subscribe');
+const axios = require('axios');
+require('dotenv').config();
 
 const execute = async (interaction) => {
-    
+    debug(`Creating axios instance for: ${interaction.user.tag}`);
+    const instance = axios.create({
+        baseURL: process.env.CAP_URL + process.env.CAP_SUBSCRIPTION_SERVICE,
+        timeout: 1000,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    try {
+        debug(`Attempting to remove: ${interaction.user.tag} in DB`);
+        const capResponse = await instance.post("unsubscribe", {ID: interaction.user.id});
+        debug(`Removal successful: ${interaction.user.tag}`);
+        await interaction.reply(`Removed ${interaction.user.tag} from subscription list`);
+    } catch(error) {
+        await interaction.reply('Something went wrong under removal');
+    }
 };
 
 module.exports = {
