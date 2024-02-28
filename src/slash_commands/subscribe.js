5824a8f41aa4ac1a202c988a3cc8838a37cf9690
Echono <wonger1324@gmail.com>
2024-02-28 13:04:47 +0100

    Created a working subscribe to CAP DB

--- src/slash_commands/subscribe.js
+++ src/slash_commands/subscribe.js
@@ -1,7 +1,37 @@
 const { SlashCommandBuilder } = require('discord.js');
+const debug = require('debug')('slashCommand_subscribe');
+const axios = require('axios');
+require('dotenv').config();
 
 const execute = async (interaction) => {
+    debug(`Subscribe was called by: ${interaction.user.tag}`);
+    const { user } = interaction;
+    const userInformation = {
+        ID: user.id,
+        tag: user.tag,
+        username: user.username
+    };
     
+    debug(`Creating axios instance for: ${interaction.user.tag}`);
+    const instance = axios.create({
+        baseURL: process.env.CAP_URL + process.env.CAP_SUBSCRIPTION_SERVICE,
+        timeout: 1000,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    try {
+        debug(`Attempting to upload details of: ${interaction.user.tag} to DB`);
+        const capResponse = await instance.post("UserSet", userInformation);
+        
+        debug(`Upload successful: ${interaction.user.tag}`);
+        await interaction.reply(`Added ${capResponse.data.tag} to subscription list`);
+    } catch(error) {
+        if(error.response.data.error.message = "Entity already exists") {
+            debug(`User: ${interaction.user.tag} already exists in DB`);
+            await interaction.reply(`User ${userInformation.tag} already exists in subscription list`);
+        }
+    }
 };
 
 module.exports = {
